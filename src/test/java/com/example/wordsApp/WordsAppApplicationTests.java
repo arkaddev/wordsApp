4e4ed7defdb9a95a2d5af23f8bb5431552a16863
arkaddev <arek1994mr@gmail.com>
2023-04-30 11:40:13 +0200

    initial commit

---
+++ src/test/java/com/example/wordsApp/WordsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wordsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
